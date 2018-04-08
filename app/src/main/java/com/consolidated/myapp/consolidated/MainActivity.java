e58723e682dc9cadcaee1eccd1b54c7e07f48ca0
erickpang-dev <erickpang_90@hotmail.com>
2018-04-08 19:32:39 -0300

    More MainActivity preparation and TextViewShowcase completion

--- app/src/main/java/com/consolidated/myapp/consolidated/MainActivity.java
+++ app/src/main/java/com/consolidated/myapp/consolidated/MainActivity.java
@@ -1,12 +1,15 @@
 package com.consolidated.myapp.consolidated;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,17 +19,17 @@ public class MainActivity extends Activity {
             "Button",
             "Array",
             "Toast",
-            "Activity navigation",
-            "Activity information transfer",
-            "Listview",
-            "Alert dialog",
+            "Activity Navigation",
+            "Activity Information Transfer",
+            "ListView",
+            "Alert Dialog",
             "Checkbox",
-            "Seekbar",
-            "Radio button",
-            "Toggle button",
-            "Media player",
-            "Shared preference",
-            "Internal storage"
+            "SeekBar",
+            "Radio Button",
+            "Toggle Button",
+            "Media Player",
+            "Shared Preference",
+            "Internal Storage"
     };
 
     @Override
@@ -47,8 +50,59 @@ public class MainActivity extends Activity {
         activityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                
+                Log.v("MainActivity", "Item clicked: " + i);
+                switch (i){
+                    case 0: Intent newActivity = new Intent(MainActivity.this, TextViewShowcase.class);
+                            startActivity(newActivity);
+                    break;
+//                    case 1: Intent newActivity = new Intent(MainActivity.this, ButtonShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 2: Intent newActivity = new Intent(MainActivity.this, ArrayShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 3: Intent newActivity = new Intent(MainActivity.this, ToastShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 4: Intent newActivity = new Intent(MainActivity.this, ActivityNavigationShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 5: Intent newActivity = new Intent(MainActivity.this, ActivityInformationTransferShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 6: Intent newActivity = new Intent(MainActivity.this, ListViewShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 7: Intent newActivity = new Intent(MainActivity.this, AlertDialogShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 8: Intent newActivity = new Intent(MainActivity.this, CheckboxShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 9: Intent newActivity = new Intent(MainActivity.this, SeekBarShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 10: Intent newActivity = new Intent(MainActivity.this, RadioButtonShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 11: Intent newActivity = new Intent(MainActivity.this, ToggleButtonShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 12: Intent newActivity = new Intent(MainActivity.this, MediaPlayerShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 13: Intent newActivity = new Intent(MainActivity.this, SharedPreferenceShowcase.class);
+//                            startActivity(newActivity);
+//                    break;
+//                    case 14: Intent newActivity = new Intent(MainActivity.this, InternalStorageShowcase.class);
+
+                    default:
+                        Toast.makeText(MainActivity.this, "Activity not created or linked", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
+
+
 }
--- app/src/main/java/com/consolidated/myapp/consolidated/TextViewShowcase.java
+++ app/src/main/java/com/consolidated/myapp/consolidated/TextViewShowcase.java
@@ -1,14 +1,30 @@
 package com.consolidated.myapp.consolidated;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TextViewShowcase extends Activity {
+
+    private Button returnButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view_showcase);
+
+        returnButton = findViewById(R.id.returnButtonId);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TextViewShowcase.this,"Going back.",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
     }
 }
