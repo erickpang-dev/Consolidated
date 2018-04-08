f60d5b87f728d76564d796c7a4934cd4b822dabc
erickpang-dev <erickpang_90@hotmail.com>
2018-04-08 17:47:50 -0300

    MainActivity preparation and TextViewShowcase creation

--- app/src/main/java/com/consolidated/myapp/consolidated/MainActivity.java
+++ app/src/main/java/com/consolidated/myapp/consolidated/MainActivity.java
@@ -3,12 +3,52 @@ package com.consolidated.myapp.consolidated;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    private ListView activityList;
+    private String[] activitiesNames = {
+            "TextView",
+            "Button",
+            "Array",
+            "Toast",
+            "Activity navigation",
+            "Activity information transfer",
+            "Listview",
+            "Alert dialog",
+            "Checkbox",
+            "Seekbar",
+            "Radio button",
+            "Toggle button",
+            "Media player",
+            "Shared preference",
+            "Internal storage"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityList = findViewById(R.id.activityListId);
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                activitiesNames
+        );
+
+        activityList.setAdapter(namesAdapter);
+        activityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/consolidated/myapp/consolidated/TextViewShowcase.java
@@ -0,0 +1,14 @@
+package com.consolidated.myapp.consolidated;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextViewShowcase extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view_showcase);
+    }
+}
