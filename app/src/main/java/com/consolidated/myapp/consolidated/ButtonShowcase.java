188b96a786822b483d38a4c86a88c676f8724fd6
erickpang-dev <erickpang_90@hotmail.com>
2018-04-08 19:59:53 -0300

    Creation of strings in strings.xml and ButtonShowcase Activity

---
+++ app/src/main/java/com/consolidated/myapp/consolidated/ButtonShowcase.java
@@ -0,0 +1,13 @@
+package com.consolidated.myapp.consolidated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ButtonShowcase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_showcase);
+    }
+}
