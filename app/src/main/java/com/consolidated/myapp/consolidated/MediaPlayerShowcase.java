9400e01ec81b51ba296ae9afe387643ee4c53f86
erickpang-dev <erickpang.dev@gmail.com>
2018-04-13 23:03:18 -0300

    MediaPlayerShowcase activity creation

---
+++ app/src/main/java/com/consolidated/myapp/consolidated/MediaPlayerShowcase.java
@@ -0,0 +1,14 @@
+package com.consolidated.myapp.consolidated;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaPlayerShowcase extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_player_showcase);
+    }
+}
